188990dcffd6e6e7c8514a7432d6ec16adf09e9e
Jarrett Kong <jarrettkong@me.com>
2019-05-08 15:49:47 -0600

    Wrap poster in Link

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import './_Poster.scss';
 
 const Poster = props => {
 	return (
+		<Link to={`/movies/${props.id}`} key={props.id}>
 		<article className="Poster">
 			<img src={props.poster} alt={`${props.title} poster`} className="Poster-poster" />
 			<h3 className="Poster-title">{props.title}</h3>
 		</article>
+		</Link>
 	);
 };
 
