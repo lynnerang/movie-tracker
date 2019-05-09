29d890c0c4370892085288a62227e82f5ee77783
Lynne Rang <lynne.rang@gmail.com>
2019-05-09 13:36:29 -0600

    Add actions for user login and logout.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { trendingMoviesReducer } from './trendingMoviesReducer';
 import { topRatedMoviesReducer } from './topRatedMoviesReducer';
 import { nowPlayingMoviesReducer } from './nowPlayingMoviesReducer';
 import { upcomingMoviesReducer } from './upcomingMoviesReducer';
+import { userReducer } from './userReducer';
 
 const rootReducer = combineReducers({
   trendingMovies: trendingMoviesReducer,
   topRatedMovies: topRatedMoviesReducer,
   nowPlayingMovies: nowPlayingMoviesReducer,
-  upcomingMovies: upcomingMoviesReducer
+  upcomingMovies: upcomingMoviesReducer,
+  user: userReducer
 });
 
 export default rootReducer;
