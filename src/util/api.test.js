508095dc6dc64a328f5b80c55a31b55838dc0e06
Lynne Rang <lynne.rang@gmail.com>
2019-05-11 13:20:39 -0600

    Add passing tests for user fetch alongside existing movies fetch.

--- src/util/api.test.js
+++ src/util/api.test.js
@@ -1,5 +1,5 @@
 import * as Api from './Api';
-import { mockMovie } from './mockData/mockData';
+import { mockMovie, mockUser, mockPost } from './mockData/mockData';
 
 describe('fetchMovies', () => {
   beforeEach(() => {
@@ -24,6 +24,33 @@ describe('fetchMovies', () => {
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: false
     }));
-    await expect(Api.fetchMovies()).rejects.toEqual(Error('Can\'t fetch movies.'))
+    await expect(Api.fetchMovies()).rejects.toEqual(Error('Failed to fetch movie data.'))
+  })
+})
+
+describe('fetchUser', () => {
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve(mockUser)
+    }));
+  })
+
+  it('should call fetch with the correct arguments', async () => {
+    const expectedUrl = 'http://localhost:3000/api/users/';
+    await Api.fetchUser({ email: mockUser.email, password: mockUser.password }, '');
+    expect(window.fetch).toHaveBeenCalledWith(expectedUrl, mockPost);
+  })
+
+  it('should return a parsed version of the result', async () => {
+    const expectedData = await Api.fetchUser();
+    expect(expectedData).toEqual(mockUser);
+  })
+
+  it('should throw an error if the fetch isn\'t successful', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: false
+    }));
+    await expect(Api.fetchUser()).rejects.toEqual(Error('Failed to post user data.'))
   })
 })
