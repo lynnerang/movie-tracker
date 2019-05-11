1d4e0b1e5cd81b33eac34e1800fee102e1f6397d
Lynne Rang <lynne.rang@gmail.com>
2019-05-11 14:44:36 -0600

    Convert App to a functional component.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,32 +7,30 @@ import HomeScreen from '../../containers/HomeScreen/HomeScreen';
 import { Switch, Route } from 'react-router-dom';
 import './_App.scss';
 
-class App extends Component {
-	render() {
-		return (
-			<section className="App">
-				<Header />
-				<div className="below-header">
-					<Navigation />
-					<main className="main-content">
-						<Switch>
-              <Route exact path="/" component={HomeScreen} />
-              <Route exact
-                     path="/category/:id"
-                     component={({ match }) => <ResultsScreen section={match.params.id} />}
-              />
-							<Route
-								exact
-								path="/movies/:id"
-								component={({ match }) => <MovieDetails id={match.params.id} key={match.params.id} />}
-							/>
-							{/* <Route exact path="/movies/:id" component={MovieDetails} /> */}
-						</Switch>
-					</main>
-				</div>
-			</section>
-		);
-	}
+const App = () => {
+  return (
+    <section className="App">
+      <Header />
+      <div className="below-header">
+        <Navigation />
+        <main className="main-content">
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route exact
+                    path="/category/:id"
+                    component={({ match }) => <ResultsScreen section={match.params.id} />}
+            />
+            <Route
+              exact
+              path="/movies/:id"
+              component={({ match }) => <MovieDetails id={match.params.id} key={match.params.id} />}
+            />
+            {/* <Route exact path="/movies/:id" component={MovieDetails} /> */}
+          </Switch>
+        </main>
+      </div>
+    </section>
+  );
 }
 
 export default App;
