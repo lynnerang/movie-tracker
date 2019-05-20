c196725c777e5d8aa031c69c9a18694d6b88f465
Lynne Rang <lynne.rang@gmail.com>
2019-05-19 19:55:41 -0600

    Updates

---
+++ precache-manifest.11258db7f7819baeb3aae81b2f7f2b89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4badc3719bef03dfc502f56c667371ba",
+    "url": "/movie-tracker/index.html"
+  },
+  {
+    "revision": "035cc956366c4349c7aa",
+    "url": "/movie-tracker/static/css/main.e6359c34.chunk.css"
+  },
+  {
+    "revision": "4a43f74a8f05c489d4f7",
+    "url": "/movie-tracker/static/js/2.4ceb28a8.chunk.js"
+  },
+  {
+    "revision": "035cc956366c4349c7aa",
+    "url": "/movie-tracker/static/js/main.c203ff91.chunk.js"
+  },
+  {
+    "revision": "def1329353ad87605c3a",
+    "url": "/movie-tracker/static/js/runtime~main.e9d78e93.js"
+  },
+  {
+    "revision": "23ac4eb2d80514456d8643cb89099de8",
+    "url": "/movie-tracker/static/media/logo.23ac4eb2.png"
+  },
+  {
+    "revision": "47ad8e9a16ee0f2c969d854f0b4018dd",
+    "url": "/movie-tracker/static/media/main-bg.47ad8e9a.png"
+  }
+]);
